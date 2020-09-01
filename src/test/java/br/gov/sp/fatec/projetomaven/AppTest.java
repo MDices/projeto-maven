95e62facf27404ed19e2563032b1122f650c079c
MDices <lgs.mttp@gmail.com>
2020-09-01 19:30:34 -0300

    att_testCalc

--- src/test/java/br/gov/sp/fatec/projetomaven/AppTest.java
+++ src/test/java/br/gov/sp/fatec/projetomaven/AppTest.java
@@ -14,12 +14,28 @@ public class AppTest
      * Rigourous Test :-)
      */
    @Test
-	public void testCalculadora()
+	public void testSoma()
     {
        calculadora calc = new calculadora(); 
 	   assertTrue(calc.soma(1, 1) == 2);
-	   assertTrue(calc.subtracao(1, 1) == 0);
-	   assertTrue(calc.multiplicacao(1, 1) == 1);
-	   assertTrue(calc.divisao(1, 1) == 1);
+	
     }
+   @Test
+   public void testSub()
+   {
+      calculadora calc = new calculadora(); 
+	   assertTrue(calc.subtracao(1, 1) == 0);
+   }
+   @Test
+   public void testMulti()
+   {
+	   calculadora calc = new calculadora();
+	   assertTrue(calc.multiplicacao(1, 1) == 1);
+   
+   }
+   @Test
+   public void testDiv() {
+	   calculadora calc = new calculadora();
+	   assertTrue(calc.divisao(1, 1) == 1);
+   }
 }
